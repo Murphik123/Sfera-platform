cfde076819475fa76a4cad1c1a2ac7cc5fc94e8d
Murphik123 <roman.ashabad@gmail.com>
2026-09-25 12:02:28 +0500

    Delete backend directory

--- backend/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getMessages, sendMessage } = require('../controllers/chatController');
-
-router.get('/', auth, getMessages);
-router.post('/', auth, sendMessage);
-
-module.exports = router;
